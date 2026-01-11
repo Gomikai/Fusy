52c9e93498d9ec3903345c3994a55c86dabc3a1a
Yuanjie Fu <tthendofinossomin@gmail.com>
2026-01-10 19:19:18 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,11 @@
 const items = [
     {
         src: 'https://photos.app.goo.gl/dqUYb7raoctpj8186',
-        description: '## 干锅花菜\n- 测试'
+        description: '## 干锅花菜\n- 花菜粗的根尽量对半切，焯水1分钟，可加盐油\n -煸炒培根片出油，加蒜末等\n -下花菜，加生抽胡椒'
+    },
+    {
+        src: 'https://photos.app.goo.gl/ozcHd2ZttHfuVt5h9',
+        description: '## 麻婆豆腐\n软豆腐经典口感好，老豆腐入味，避开silken tofu会变成豆腐脑\n- 豆腐切小块加盐焯水\n- 炒牛肉末，加豆瓣辣椒面，真辣椒切小圈加辣装饰\n- 加一抽水烧开，下豆腐，加生抽\n -水不宜多，勾芡玉米淀粉不宜少，多次加，轻推搅拌',
     },
     {
         src: 'https://photos.app.goo.gl/LrtNVkPxJSEP5TP28',
