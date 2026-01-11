7cba0f11b4c01bb50ec5fb076886dfbd2a9629f4
GomiKai <tthendofinossomin@gmail.com>
2026-01-10 19:42:19 -0500

    FU

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 const items = [
     {
         src: 'https://photos.app.goo.gl/dqUYb7raoctpj8186',
-        description: '## 干锅花菜
+        description: `## 干锅花菜
 
 花菜粗的根尽量对半切，焯水1分钟，可加盐油
 
 - 煸炒培根片出油，加蒜末等
-- 下花菜，加生抽胡椒'
+- 下花菜，加生抽胡椒`
     },
     {
         src: 'https://photos.app.goo.gl/ozcHd2ZttHfuVt5h9',
-        description: '## 麻婆豆腐
+        description: `## 麻婆豆腐
 
 软豆腐经典口感好，老豆腐入味，避开silken tofu会变成豆腐脑
 
 - 豆腐切小块加盐焯水
 - 炒牛肉末，加豆瓣辣椒面，真辣椒切小圈加辣装饰
 - 加一抽水烧开，下豆腐，加生抽
-- 水不宜多，勾芡玉米淀粉不宜少，多次加，轻推搅拌'
+- 水不宜多，勾芡玉米淀粉不宜少，多次加，轻推搅拌`
     },
     {
         src: 'https://photos.app.goo.gl/LrtNVkPxJSEP5TP28',
-        description: '## 茅腿'
+        description: `## 茅腿`
     }
 ];
